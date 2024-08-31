482ee11a1166873c8b2f9b0f8c00e77d89544a17
Dennis Smith <denniskofismith@gmail.com>
2024-08-31 12:47:48 +0000

    mqtt with python

--- mosquitto/read.py
+++ mosquitto/read.py
@@ -4,4 +4,21 @@ def on_connect(client,userdata,flags,rc):
     print(f"Connected with result code {str(rc)} ")
 
 
-    client.subscribe("")
+    client.subscribe("koddy")
+
+# def on_message(client,userdata, msg):
+#     print(f"{msg.topic} {\n}  msg.payload.decode("utf-8") {\n}")
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " \n " + msg.payload.decode("utf-8") + " \n ")
+
+    
+
+client = mqtt.Client()
+
+client.on_message = on_message
+client.on_connect = on_connect
+
+client.connect("0.0.0.0", 1883, 60)
+
+client.loop_forever()
